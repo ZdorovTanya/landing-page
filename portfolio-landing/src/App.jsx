0294d57f76dc11d7dd3f98e924ac547f9fdb13a2
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-07-03 22:20:57 +0300

    start of the project

---
+++ portfolio-landing/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./css/App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>jjfjf</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
