2a4ca90d2927e473105519759c5e5dd7b197e79f
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-07-04 17:03:15 +0300

    grid with button without tailwind

---
+++ portfolio-landing/src/js/burger.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.icon-menu').click(function(event){
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+});
